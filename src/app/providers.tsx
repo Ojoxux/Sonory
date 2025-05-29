c71dc9cd7a4162c9d6c263278b1e218f083d56e2
Jou Okuyama <ojoxux@gmail.com>
2025-05-29 11:20:00 +0900

    refactor: ♻️ ChakraProviderのラップを削除

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,13 @@
 'use client'
 
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { PropsWithChildren } from 'react'
 
+/**
+ * アプリケーション全体のプロバイダー
+ *
+ * @param children - 子コンポーネント
+ * @returns プロバイダーでラップされた子コンポーネント
+ */
 export function Providers({ children }: PropsWithChildren) {
-  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+  return <>{children}</>
 }
