417910ede5404d2232c240490a61a03f8a997379
Jou Okuyama <ojoxux@gmail.com>
2025-05-29 11:21:45 +0900

    feat(home): ✨ ホーム画面コンポーネントの更新

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { UIOverlay } from '@/components/UIOverlay'
-import { Box } from '@chakra-ui/react'
+import { useGeolocation } from '@/components/organisms/MapComponent/hooks/useGeolocation'
+import { UIOverlay } from '@/components/organisms/UIOverlay'
 import dynamic from 'next/dynamic'
+import type { ReactElement } from 'react'
 
 // MapComponentをクライアントサイドのみでロードするために動的インポート（SSRなし）
 const MapComponent = dynamic(
-  () => import('@/components/MapComponent').then((mod) => mod.MapComponent),
+  () =>
+    import('@/components/organisms/MapComponent').then(
+      (mod) => mod.MapComponent,
+    ),
   { ssr: false },
 )
 
@@ -14,32 +18,29 @@ const MapComponent = dynamic(
  * Sonoryのホーム画面コンポーネント
  *
  * フルスクリーンマップとUIオーバーレイを表示する
+ *
+ * @returns ホーム画面のJSX要素
  */
-export default function Home() {
+export default function Home(): ReactElement {
+  const { position } = useGeolocation()
+
+  const handleSettingsClick = (): void => {
+    console.log('設定ボタンがクリックされました')
+    // TODO: 設定画面の表示処理を実装
+  }
+
   return (
-    <Box position="relative" h="100vh" w="100vw" overflow="hidden">
-      <Box
-        position="absolute"
-        top="0"
-        left="0"
-        right="0"
-        bottom="0"
-        zIndex="0"
-        pointerEvents="auto"
-      >
+    <div className="relative h-screen w-screen overflow-hidden">
+      <div className="absolute inset-0 z-0 pointer-events-auto">
         <MapComponent />
-      </Box>
-      <Box
-        position="absolute"
-        top="0"
-        left="0"
-        right="0"
-        bottom="0"
-        zIndex="10"
-        pointerEvents="none"
-      >
-        <UIOverlay />
-      </Box>
-    </Box>
+      </div>
+      <div className="absolute inset-0 z-10 pointer-events-none">
+        <UIOverlay
+          onSettingsClick={handleSettingsClick}
+          latitude={position?.latitude}
+          longitude={position?.longitude}
+        />
+      </div>
+    </div>
   )
 }
