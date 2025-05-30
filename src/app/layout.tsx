4c138efea69834d71191ae261cad14f0c9d72136
Jou Okuyama <ojoxux@gmail.com>
2025-05-30 13:01:59 +0900

    feat: ✨ NunitoとM Plus RoundedフォントをRootLayout追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, M_PLUS_Rounded_1c, Nunito } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
 
@@ -11,6 +11,19 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+})
+
+const nunito = Nunito({
+  variable: '--font-nunito',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+})
+
+const mPlusRounded = M_PLUS_Rounded_1c({
+  variable: '--font-mplus-rounded',
+  weight: ['400', '500', '700', '800', '900'],
+  preload: false,
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -26,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} ${mPlusRounded.variable} antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
