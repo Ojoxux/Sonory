e7e91292b51cec52d5bc4333675bb3f07fdfeade
NCC_TAIKI_YOSHIDA <taiyakiboy18201803@gmail.com>
2025-06-19 16:05:08 +0900

    00:09から00:10に修正その2

--- src/components/molecules/WaveformPlayer/utils.ts
+++ src/components/molecules/WaveformPlayer/utils.ts
@@ -51,7 +51,7 @@ export function extractWaveformPeaks(
  */
 export function formatTime(timeInSeconds: number): string {
   const minutes = Math.floor(timeInSeconds / 60)
-  const seconds = Math.floor(timeInSeconds % 60)
+  const seconds = Math.round(timeInSeconds % 60)
   return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
