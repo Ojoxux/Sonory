f46a2ba4738ed7ad4588227501fb9b223debdaa8
NCC_TAIKI_YOSHIDA <taiyakiboy18201803@gmail.com>
2025-06-19 14:55:03 +0900

    録音完了後の最大再生時間の表記を00:09から00:10に修正

--- src/components/molecules/WaveformPlayer/index.tsx
+++ src/components/molecules/WaveformPlayer/index.tsx
@@ -397,7 +397,7 @@ export function WaveformPlayer({
    */
   const formatTime = useCallback((time: number): string => {
     const minutes = Math.floor(time / 60)
-    const seconds = Math.floor(time % 60)
+    const seconds = Math.round(time % 60)
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   }, [])
 
