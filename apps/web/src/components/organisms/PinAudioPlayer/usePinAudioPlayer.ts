7c32ee316241fda95d8570de699a0229b89c53a8
Jou Okuyama <ojoxux@gmail.com>
2025-10-08 11:55:19 +0900

    feat: dateFormatユーティリティをインポート

--- apps/web/src/components/organisms/PinAudioPlayer/usePinAudioPlayer.ts
+++ apps/web/src/components/organisms/PinAudioPlayer/usePinAudioPlayer.ts
@@ -1,10 +1,11 @@
 import type { SoundPin } from "@/store/useSoundPinStore"
+import { formatRecordedAt, formatTime } from "@/utils/dateFormat"
 import { useCallback, useEffect, useRef, useState } from "react"
 import { useEffectEvent } from "use-effect-event"
 import type {
-   AudioLoadingStatus,
-   PlaybackState,
-   UsePinAudioPlayerReturn,
+	AudioLoadingStatus,
+	PlaybackState,
+	UsePinAudioPlayerReturn,
 } from "./types"
 
 /**
