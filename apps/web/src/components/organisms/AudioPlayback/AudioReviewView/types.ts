546ddd4dd92e3b97bc0c6726b1bf904bb8d3b990
Jou Okuyama <ojoxux@gmail.com>
2025-10-08 11:29:08 +0900

    refactor: AudioReviewView の型定義を作成

---
+++ apps/web/src/components/organisms/AudioPlayback/AudioReviewView/types.ts
@@ -0,0 +1,23 @@
+import type { AudioData } from "@/store/types"
+
+/**
+ * AudioReviewView コンポーネントのプロパティ型
+ *
+ * @description
+ * 録音確認画面のプロパティ
+ */
+export type AudioReviewViewProps = {
+	/** 再生する音声データ */
+	audioData: AudioData
+	/** 録音日時のフォーマット済み文字列 */
+	formattedDate: string
+	/** 続けるボタンのクリックハンドラー */
+	onContinue: () => void
+	/** キャンセルボタンのクリックハンドラー */
+	onCancel: () => void
+	/** 波形プレイヤーの準備完了時のコールバック */
+	onWaveformReady?: () => void
+	/** 波形プレイヤーの再生完了時のコールバック */
+	onWaveformFinish?: () => void
+}
+
